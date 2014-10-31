4cab06d329e310999c6d0724773335314bb2d444
Gustavo Luvizotto Cesar <gustavoluvizotto@gmail.com>
2014-10-30 22:19:50 -0200

    Trying to read the wav file.

---
+++ inc/wav.h
@@ -0,0 +1,41 @@
+/*
+ * wav.h
+ *
+ *  Created on: Oct 30, 2014
+ *      Author: gustavo
+ */
+
+#ifndef INC_WAV_H_
+#define INC_WAV_H_
+
+#include <stdint.h>
+
+#pragma pack (push, 1) 	/* exact fit; without padding*/
+typedef struct {
+	char chunk_id[4];           /* big      */
+	uint32_t chunk_size;        /* little   */
+	char format[8];             /* big      */
+} riff_chunk_t;
+#pragma pack (pop)		/* back to the normal format pack mode */
+
+#pragma pack (push, 1)
+typedef struct {
+	char sub_chunk1_id[4];      /* big      */
+    uint32_t sub_chunk1_size;   /* little   */
+    uint16_t audio_format;      /* little   */
+    uint16_t num_channels;      /* little   */
+    uint32_t sample_rate;       /* little   */
+    uint32_t byte_rate;         /* little   */
+    uint16_t block_align;       /* little   */
+    uint16_t bits_per_sample;   /* little   */
+} fmt_chunk_t;
+#pragma pack (pop)
+
+#pragma pack (push, 1)
+typedef struct {
+	char sub_chunk2_id[4];      /* big      */
+    uint32_t sub_chunk2_size;   /* little   */
+    uint8_t *data;              /* little   */
+} data_chunk_t;
+
+#endif /* INC_WAV_H_ */
--- main.c
+++ main.c
@@ -6,6 +6,52 @@
  */
 
 #include <stdio.h>
+#include "inc/wav.h"
+
+#undef	ERR_NO
+#define ERR_NO 0
+
+#undef	ERR_FAIL
+#define	ERR_FAIL 1
+
+typedef int8_t result_t;
+
+/* global */
+riff_chunk_t riff_chunk;
+fmt_chunk_t fmt_chunk;
+data_chunk_t data_chunk;
+
+result_t read_sound(char *in_file, FILE *fp) {
+	result_t result = -ERR_NO;
+	size_t reading;
+
+	fp = fopen(in_file, "rb");
+	if (fp == NULL) {
+		goto FAIL_OPEN_FILE;
+	}
+
+	/* reads header */
+	reading = fread(&riff_chunk, sizeof(riff_chunk), 1, fp);
+	if (reading != 1) {
+		goto FAIL_READ_FILE;
+	}
+	reading = fread(&fmt_chunk, sizeof(fmt_chunk), 1, fp);
+	if (reading != 1) {
+		goto FAIL_READ_FILE;
+	}
+	reading = fread(&data_chunk, sizeof(data_chunk), 1, fp);
+	if (reading != 1) {
+		goto FAIL_READ_FILE;
+	}
+
+	result = -ERR_NO;
+	return result;
+
+FAIL_OPEN_FILE:
+FAIL_READ_FILE:
+	result = -ERR_FAIL;
+	return result;
+}
 
 int main (int argc, char* argv[]) {
 
